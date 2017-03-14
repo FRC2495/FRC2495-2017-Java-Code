848b064a0b1cd77ae9ebd45286672163afd52e7b
Frc2495 <FRC2495@gmail.com>
2017-03-14 14:23:02 -0400

    Actually added some code this time relating to the camera

--- 2495 Attempt Numero Uno/src/org/usfirst/frc/team2495/robot/HMCamera.java
+++ 2495 Attempt Numero Uno/src/org/usfirst/frc/team2495/robot/HMCamera.java
@@ -2,84 +2,100 @@ package org.usfirst.frc.team2495.robot;
 
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
-public class HMCamera{
+public class HMCamera {
 
 	NetworkTable nt;
 	double[] area, width, height, centerX, centerY;
-	
+
 	private static final int MAX_NT_RETRY = 5;
 
 	public HMCamera(String networktable) {
 		nt = NetworkTable.getTable(networktable);
 	}
 
-	private void setLocalTables(double[] area, double[] width, double[] height, double[] centerX,  double[] centerY) {
+	private void setLocalTables(double[] area, double[] width, double[] height, double[] centerX, double[] centerY) {
 		this.area = area;
 		this.width = width;
 		this.height = height;
 		this.centerX = centerX;
 		this.centerY = centerY;
 	}
-	
+
 	private void updateFromNT() {
-		double[] def = {}; // Return an empty array by default.		
+		double[] def = {}; // Return an empty array by default.
 		int retry_count = 0;
-		setLocalTables(null, null, null, null, null);	
+		setLocalTables(null, null, null, null, null);
 
 		// We cannot get arrays atomically but at least we can make sure they
 		// have the same size
 		do {
 			// Get data from NetworkTable
-			setLocalTables(
-					nt.getNumberArray("area", def),
-					nt.getNumberArray("width", def),
-					nt.getNumberArray("height", def),
-					nt.getNumberArray("centerX", def),
+			setLocalTables(nt.getNumberArray("area", def), nt.getNumberArray("width", def),
+					nt.getNumberArray("height", def), nt.getNumberArray("centerX", def),
 					nt.getNumberArray("centerY", def));
 
 			retry_count++;
 		} while (!isCoherent() && retry_count < MAX_NT_RETRY);
 	}
-	
+
 	private void processInformation() {
+		int largeAIndex, largeBIndex = 0;
+		double[] areaSave = area;
+		if (areaSave.length >= 2) {
+			largeAIndex = 0;
+			for (int i = 1; i < areaSave.length; i++) {
+				if (areaSave[i] > areaSave[largeAIndex]) {
+					largeAIndex = i;
+				}
+			}
+
+			for (int i = 0; i < areaSave.length; i++) {
+				if (areaSave[i] > areaSave[largeBIndex] && i != largeAIndex) {
+					largeBIndex = i;
+				}
+			}
+		}
+
 		// step 1: verify we have at least two targets
-		
+
 		// step 2: get the index of the largest two targets
-		
-		// step 3: calculate the bounding box of the largest two targets (i.e. the small rectangle that encompasses the largest two targets)
-		
-		// step 4: calculate the distance to the virtual target represented by the bounding box of the largest two targets
-		
-		// step 5: calculate the angle delta between the current robot heading and the center of the virtual target		
+
+		// step 3: calculate the bounding box of the largest two targets (i.e.
+		// the small rectangle that encompasses the largest two targets)
+
+		// step 4: calculate the distance to the virtual target represented by
+		// the bounding box of the largest two targets
+
+		// step 5: calculate the angle delta between the current robot heading
+		// and the center of the virtual target
 	}
-	
+
 	public boolean isCoherent() {
-		return (area != null && width != null && height != null && centerX != null
-				&& centerY != null && area.length == width.length
-				&& area.length == height.length && area.length == centerX.length
+		return (area != null && width != null && height != null && centerX != null && centerY != null
+				&& area.length == width.length && area.length == height.length && area.length == centerX.length
 				&& area.length == centerY.length);
 	}
-	
-	public int getNumberOfTargets()
-	{
+
+	public int getNumberOfTargets() {
 		if (isCoherent()) {
-			return area.length; // all tables have the same size so any length can be used (might be zero)
+			return area.length; // all tables have the same size so any length
+								// can be used (might be zero)
 		} else {
 			return 0; // best answer in that case
 		}
-	}	
-	
+	}
+
 	public boolean acquireTargets() {
 		updateFromNT(); // gets the latest info
 
-		if (isCoherent() && getNumberOfTargets() > 0) { // if we have targets 
+		if (isCoherent() && getNumberOfTargets() > 0) { // if we have targets
 			processInformation();
 			return true;
 		} else {
 			return false;
 		}
 	}
- 
+
 	public boolean checkForGearLift() {
 		return getNumberOfTargets() > 1; // gear lift is at least two targets
 	}
